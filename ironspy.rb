de29ef0affa654fd09500f5c3d1bde430b08ca89
Eren Kızılay <32452198+ErenKizilay@users.noreply.github.com>
2025-06-27 14:12:15 +0300

    Updates checksum of tar.gz

--- ironspy.rb
+++ ironspy.rb
@@ -2,7 +2,7 @@ class Ironspy < Formula
   desc "AWS resource scanner"
   homepage "https://github.com/yourusername/ironspy"
   url "https://github.com/ErenKizilay/homebrew-ironspy/archive/refs/tags/v1.0.0-snapshot/ironspy.tar.gz"
-  sha256 "debb7d1dd6c198966e1b48faf7f9793d4b53af83cd93c227e60f3518b1e59cda"
+  sha256 "189c1c5880df6c596cc87f6493b3b06c33c687d79e9b6d5e5b9e2a04d2b70083"
   license "MIT"
   version "1.0.0-snapshot"
 
